41d6efb52824b72aa929dcc86fd8baecbb57f7bb
Ricardo Haas <ricardohaas@msn.com>
2024-12-25 16:53:45 -0300

    Adjust example

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
+          Review next step of this tutorial hey.
         </a>
       </div>
     </main>
